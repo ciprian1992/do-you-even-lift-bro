3bac43cc60a5795e6856c8974f57aea6f7ccc47a
unknown <tarlevcristina@gmail.com>
2024-04-08 23:18:28 +0200

    second commit

--- src/app/max-weight-container/max-weight-container.module.ts
+++ src/app/max-weight-container/max-weight-container.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { MaxWeightContainerComponent } from './max-weight-container.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule],
+  imports: [CommonModule, IonicModule],
   declarations: [MaxWeightContainerComponent],
   exports: [MaxWeightContainerComponent],
 })
