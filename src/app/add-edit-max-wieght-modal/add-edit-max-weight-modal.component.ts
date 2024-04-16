61e1c53b77947d3c8666afc4c8fa72cfa11c0c60
unknown <tarlevcristina@gmail.com>
2024-04-16 20:10:06 +0200

    add max weight logic

--- src/app/add-edit-max-wieght-modal/add-edit-max-weight-modal.component.ts
+++ src/app/add-edit-max-wieght-modal/add-edit-max-weight-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { IonicModule, ModalController } from '@ionic/angular';
 import { MuscleGroup, muscleGroups } from './musle-groups.const';
 import { CommonModule } from '@angular/common';
@@ -8,8 +8,22 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { Observable, map, shareReplay } from 'rxjs';
+import {
+  Observable,
+  Subject,
+  Subscription,
+  catchError,
+  map,
+  of,
+  shareReplay,
+  switchMap,
+  withLatestFrom,
+} from 'rxjs';
 import { Exercise, EXERCISES } from './exercises.const';
+
+import { from } from 'rxjs';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Auth, user } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-add-edit-max-weight-modal',
@@ -17,18 +31,23 @@ import { Exercise, EXERCISES } from './exercises.const';
   standalone: true,
   imports: [CommonModule, IonicModule, ReactiveFormsModule],
 })
-export class AddEditMaxWeightModalComponent {
+export class AddEditMaxWeightModalComponent implements OnInit, OnDestroy {
   public MUSCLE_GROUPS = muscleGroups;
   public formGroup = new FormGroup({
     muscleGroup: new FormControl<MuscleGroup | null>(null, Validators.required),
     exercise: new FormControl(null, Validators.required),
-    maxWeight: new FormControl(0),
-    reps: new FormControl(0),
+    maxWeight: new FormControl(),
+    reps: new FormControl(),
     info: new FormControl(''),
   });
   public exercises$: Observable<Exercise[]>;
 
+  private auth = inject(Auth);
+  private confirmSubject = new Subject<void>();
   private modalCtrl = inject(ModalController);
+  private firestore = inject(Firestore);
+  private subscriptions = new Subscription();
+  private userId$ = user(this.auth).pipe(map((user) => user?.uid ?? ''));
 
   constructor() {
     this.exercises$ = this.formGroup.controls.muscleGroup.valueChanges.pipe(
@@ -39,8 +58,49 @@ export class AddEditMaxWeightModalComponent {
       shareReplay(1)
     );
   }
+  public ngOnInit(): void {
+    this.subscriptions.add(this.closeOnAddEdit());
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  public confirm() {
+    if (this.formGroup.valid) {
+      this.confirmSubject.next();
+    }
+  }
 
   public closeModal() {
     this.modalCtrl.dismiss();
+  }
+
+  private closeOnAddEdit(): Subscription {
+    return this.confirmSubject
+      .pipe(
+        withLatestFrom(this.formGroup.valueChanges, this.userId$),
+        switchMap(([, formGroup, userId]) => {
+          const collectionRef = collection(
+            this.firestore,
+            'users',
+            userId,
+            'max-weights'
+          );
+
+          return from(
+            addDoc(collectionRef, {
+              name: formGroup.exercise,
+              muscleGroup: formGroup.muscleGroup,
+              maxWeight: formGroup.maxWeight,
+              reps: formGroup.reps,
+              info: formGroup.info,
+              date: new Date(),
+              // other fields
+            })
+          );
+        })
+      )
+      .subscribe(() => this.modalCtrl.dismiss());
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { PromptUpdateService } from './services/prompt-update.service';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
@@ -35,6 +36,8 @@ import { PromptUpdateService } from './services/prompt-update.service';
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    provideFirebaseApp(() => initializeApp({"projectId":"do-you-even-lift-bro-bafd1","appId":"1:497674374816:web:2a4e4bcb0d83f7a6f203c5","storageBucket":"do-you-even-lift-bro-bafd1.appspot.com","apiKey":"AIzaSyAkI9hxRIUcwuNhgjL4shdkWLMskDyV4QY","authDomain":"do-you-even-lift-bro-bafd1.firebaseapp.com","messagingSenderId":"497674374816","measurementId":"G-D2L13QKWEN"})),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
