6b1cce1352b529d8844601b8ea2011fc8f333b1f
unknown <tarlevcristina@gmail.com>
2024-05-19 00:34:26 +0200

    add loading gif

--- src/app/exercise-modal/exercise-modal.component.ts
+++ src/app/exercise-modal/exercise-modal.component.ts
@@ -2,20 +2,26 @@ import { Component, Input, OnInit, inject } from '@angular/core';
 import { IonicModule, ModalController } from '@ionic/angular';
 import { Exercise } from '../add-edit-max-wieght-modal/exercises.const';
 import { ExerciseWithInfo, exerciseType } from '../exercises/exercises.const';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-exercise-modal',
   templateUrl: 'exercise-modal.component.html',
   styleUrls: ['exercise-modal.component.scss'],
-  imports: [IonicModule],
+  imports: [CommonModule, IonicModule],
   standalone: true,
 })
 export class ExerciseModalComponent {
   @Input() public exercise?: ExerciseWithInfo;
 
+  public imageIsLoaded = false;
   private modalCtrl = inject(ModalController);
 
   public closeModal() {
     this.modalCtrl.dismiss();
+  }
+
+  public imageLoaded() {
+    this.imageIsLoaded = true;
   }
 }
